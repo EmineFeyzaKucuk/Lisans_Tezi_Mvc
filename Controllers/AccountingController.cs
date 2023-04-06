2f92774f034ef68d40c547d57a9fe3a5706b4911
EmineFeyzaKucuk <feyza243309@gmail.com>
2023-04-06 23:23:34 +0300

    23.23

---
+++ Controllers/AccountingController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lisans_Tezi_Mvc.Controllers
+{
+    public class AccountingController :Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //STOK HAREKET KAYITLARI
+
+        public IActionResult AccountingCodeDefinition()
+        {
+            return View("~/Views/Accounting/AccountingCodeDefinition/AccountingCodeDefinition.cshtml");
+        }
+
+    }
+}
