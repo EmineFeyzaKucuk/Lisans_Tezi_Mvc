e14ffa475bc45f2979ab145f1b9b0b325670a201
EmineFeyzaKucuk <feyza243309@gmail.com>
2023-05-16 23:48:04 +0300

    23.47

--- Controllers/ProductionController.cs
+++ Controllers/ProductionController.cs
@@ -18,6 +18,48 @@ namespace Lisans_Tezi_Mvc.Controllers
         {
             return View("~/Views/Production/PrescriptionRegistration/PrescriptionRegistration.cshtml");
         }
+        //ürün aşama
 
+        public IActionResult ProductStages()
+        {
+            return View(" ~/Views/Production/ProductStages/ProductStages.cshtml");
+        }
+              public IActionResult MachineIdentification()
+        {
+            return View("~/Views/Production/MachineIdentification/MachineIdentification.cshtml");
+        }
+
+
+        public IActionResult OperationDescription()
+        {
+            return View("~/Views/Production/OperationDescription/OperationDescription.cshtml");
+        }
+
+        public IActionResult OperatorTypesDescription()
+        {
+            return View("~/Views/Production/OperatorTypesDescription/OperatorTypesDescription.cshtml");
+        }
+
+        public IActionResult ProductGroupDefinitions()
+        {
+            return View("~/Views/Production/ProductGroupDefinitions/ProductGroupDefinitions.cshtml"; ");
+        }
+
+        public IActionResult ProductionOrderEntry()
+        {
+            return View("~/Views/Production/ProductionOrderEntry/ProductionOrderEntry.cshtml");
+        }
+
+        public IActionResult ProductionRecord()
+        {
+            return View("~/Views/Production/ProductionRecord/ProductionRecord.cshtml");
+        }
+
+        public IActionResult WorkstationIdentification()
+        {
+            return View("~/Views/Production/WorkstationIdentification/WorkstationIdentification.cshtml");
+        }
+
+      
     }
 }
